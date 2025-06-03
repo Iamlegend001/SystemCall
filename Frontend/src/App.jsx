7bd1bdc5cc59009d00638b5889241070244c7bba
Arka <arkapravasantra17@gmail.com>
2025-06-03 12:00:26 +0530

    first big project

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from "react";
+import axios from "./api/apiconfig";
+
+const App = () => {
+  const getProduct = async () => {
+    try {
+      const res = await axios.get("/products");
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getProduct();
+  }, []);
+  return <div>App</div>;
+};
+
+export default App;
